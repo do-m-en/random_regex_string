21fe233acf8f19e71c9d90db4accb754703fa355
Domen Vrankar <domen.vrankar@gmail.com>
2017-01-08 15:43:39 +0100

    -(mod) make whitespaces list constexpr

--- nodes/whitespace_regex_node.cpp
+++ nodes/whitespace_regex_node.cpp
@@ -1,25 +1,20 @@
 #include "whitespace_regex_node.hpp"
 
-#include <random>
+#include <array>
+
+namespace {
+  constexpr std::array<char, 5> whitespaces{'\r', '\n', '\t', '\f', ' '};
+}
 
 using rand_regex::whitespace_regex_node_;
 
-whitespace_regex_node_::whitespace_regex_node_()
-{
-  whitespaces_.push_back('\r');
-  whitespaces_.push_back('\n');
-  whitespaces_.push_back('\t');
-  whitespaces_.push_back('\f');
-  whitespaces_.push_back(' ');
-}
-
 void whitespace_regex_node_::generate(std::ostream& os, random_generator_base& random_gen)
 {
-  random_value_ = random_gen.get_random(0, whitespaces_.size() - 1);
-  os << whitespaces_[random_value_];
+  random_value_ = random_gen.get_random(0, whitespaces.size() - 1);
+  os << whitespaces[random_value_];
 }
 
 void whitespace_regex_node_::regenerate(std::ostream& os) const
 {
-  os << whitespaces_[random_value_];
+  os << whitespaces[random_value_];
 }
--- nodes/whitespace_regex_node.hpp
+++ nodes/whitespace_regex_node.hpp
@@ -1,7 +1,6 @@
 #ifndef WHITESPACE_REGEX_NODE_HPP_INCLUDED
 #define WHITESPACE_REGEX_NODE_HPP_INCLUDED
 
-#include <vector>
 #include "regex_node.hpp"
 
 namespace rand_regex {
@@ -9,12 +8,10 @@ namespace rand_regex {
 class whitespace_regex_node_ : public regex_node_ // match any whitespace character [\r\n\t\f ]
 {
 public:
-  whitespace_regex_node_();
   void generate(std::ostream& os, random_generator_base& random_gen) override;
   void regenerate(std::ostream& os) const override;
 
 private:
-  std::vector<char> whitespaces_; // TODO provide whitespaces by reference or a callback (amount, getter)...
   int random_value_;
 };
 
